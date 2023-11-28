6ffdcb9d3990b784415e4898d831adad8e407d14
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-11-29 01:33:33 +0800

    Update Tailwind CSS configuration and add DaisyUI plugin, added query functions and filtering system

---
+++ src/components/HeroSectionv2.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+
+function HeroSectionv2() {
+    return (
+        <div><main>
+            <div className="w-auto h-auto bg-black flex justify-center px-10">
+                <div className="w-full h-full lg:flex md:block">
+                    <div className="lg:w-[30%] md:w-full lg:h-full justify-center flex md:h-[20%]">
+                        <h1 className="text-[max(5vw,40px)] text-end text-white">
+                            Artist
+                            <br />
+                            <span className="font-bold">Directory</span>
+                        </h1>
+                    </div>
+                    <div className="lg:w-[70%] lg:col-span-3 md:col-span-6 md:w-screen lg:h-full md:h-fit">
+                        <div className="w-full h-full p-4 grid lg:grid-cols-3 md:grid-cols-2">
+                            <a href='/results/audiovisual' className="p-2 flex lg:w-full md:w-[50%] col-span-1 col-start-1 justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50 shrink">
+                                    <img
+                                        src="/public/Music.png"
+                                        alt="audiovisual"
+                                        className="shrink object-contain p-4"
+                                    />
+                                    <p className="text-center text-[18px]">AudioVisual Media</p>
+                                </div>
+                            </a>
+                            <div className="p-2 flex lg:w-full md:w-[50%] col-span-1 col-start-2 justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/public/Interactive media.png"
+                                        alt="interactive"
+                                        className="shrink object-contain p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Interactive Media</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Creative Services.png"
+                                        alt="interactive"
+                                        className="shrink object-contain p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Creative Services</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex-col flex justify-center items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Architecture.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Design</p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex flex-col items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Publishing.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] h-[90%] p-4"
+                                    />
+                                    <p className="text-center whitespace-normal text-[18px]">
+                                        Publishing and Printed Media
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex flex-col items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Performing Arts.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] h-[90%] p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Performing Arts</p>
+                                </div>
+                            </div>
+                            <a href='/results/visualarts' className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex flex-col items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Visual.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] h-[90%] p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Visual Arts</p>
+                                </div>
+                            </a>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex flex-col items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/PerformingArts2.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] h-[90%] p-4"
+                                    />
+                                    <p className="text-center text-[15px]">
+                                        Traditional Cultural <br /> Expressions
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="p-2 flex lg:w-full md:w-[50%] justify-center items-center">
+                                <div className="lg:p-10 md:p-0 lg:w-[16vw] flex flex-col items-center lg:h-[17vw] md:w-full md:h-full rounded-2xl bg-slate-50">
+                                    <img
+                                        src="/Visual2nd.png"
+                                        alt="interactive"
+                                        className="shrink object-contain w-[90%] h-[90%] p-4"
+                                    />
+                                    <p className="text-center text-[18px]">Cultural Sites</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+            pnpm </div>
+    )
+}
+
+export default HeroSectionv2
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
+import HeaderSection from './HeaderSection'
+import FootSection from './FootSection'
 
 
 
-function Results() {
+function Results({ routeName }) {
     const [res, setRes] = React.useState([])
 
     useEffect(() => {
@@ -12,23 +14,55 @@ function Results() {
     async function getFetch() {
         const response = await fetch('https://sheet.best/api/sheets/249b0e30-cf60-4ba8-9012-c5986052c0d1')
         const data = await response.json()
+        if (routeName === "") {
+            setRes(data)
+        }
+        else {
+            const filtered = data.filter(item => item.CreativeField_2 === routeName)
+            setRes(filtered)
+        }
         console.log(data)
-        setRes(data)
     }
 
     return (
         <div>
+            <HeaderSection />
+            <h1>{routeName}</h1>
             <h1>Test</h1>
-            {res.map((item, i) => {
-                return (
-                    <div key={i}>
-                        <h1>{item.FirstName}</h1>
-                        <h1>{item.LastName}</h1>
-                        <h1>{item.MobileNumber}</h1>
-                        <h1>{item.EmailAddress}</h1>
+            <div className='w-full h-screen flex justify-center py-4 bg-[#EFEFEF]'>
+                <div className='w-[90%] h-full flex gap-2'>
+                    <div className='w-[30%] flex flex-col gap-4 pt-4'>
+                        <h1 className='whitespace-normal text-2xl'>
+                        </h1>
+                        {routeName === "Audiovisual Media" && <h1 className='whitespace-normal text-4xl text-end'>AudioVisual <br /> <strong>Media</strong></h1>}
+                        {routeName === "Visual Arts" && <h1 className='whitespace-normal text-4xl text-end'>Visual <br /> <strong>Arts</strong></h1>}
+                        <a className='flex self-end' href="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </a>
                     </div>
-                )
-            })}
+
+                    <div className='w-[70%] grid grid-cols-2 grid-rows-2'>
+                        <Suspense fallback={<h1>Loading...</h1>}>
+                            {res.map((item, i) => {
+                                return (
+                                    <div className='card w-[80%] flex flex-col p-4' key={i}>
+                                        <img loading='lazy' src={item.profilephoto} className='mask mask-circle object-cover self-center w-32 h-32' alt={i} />
+                                        <h1 className='card-title'>{item.FirstName}</h1>
+                                        <div className='card-body'>
+                                            <h1>{item.LastName}</h1>
+                                            <h1>{item.MobileNumber}</h1>
+                                            <h1>{item.EmailAddress}</h1>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </Suspense>
+                    </div>
+                </div>
+            </div>
+            <FootSection />
         </div>
     )
 }
