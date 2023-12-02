f70c8c7ddc8922b14a122f6475f505077efaa2ca
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-12-03 03:36:12 +0800

    Release Candidate 1

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Header() {
+function Header({ loading, routeN }) {
     const [search, setSearch] = useState('')
 
     const handleSubmit = (e) => {
@@ -8,22 +8,37 @@ function Header() {
         console.log('submitted')
         console.log(search)
         window.location.href = `/results?search=${search}`
+
     }
+
+    const handleRouteSubmit = (e) => {
+        e.preventDefault()
+        console.log('submitted')
+        console.log(search)
+        window.location.href = `${window.location.pathname}?search=${search}`
+
+    }
+
+    useEffect(() => {
+        console.log(routeN)
+        console.log(window.location.pathname)
+    }, [])
 
     return (
         <div className="self-center bg-white flex w-full px-14 py-10 justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
             <img
                 loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/f8c84ca4-3e6e-4904-bb02-de97390494a1?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&"
-                className="aspect-[2.37] object-contain object-center w-[97px] overflow-hidden shrink-0 max-w-full"
+                src="/Creative Legazpi_LogoOutline.svg"
+                className="aspect-[2.37] object-contain object-center w-[127px] overflow-hidden shrink-0 max-w-full"
             />
             <div className="self-stretch flex items-stretch justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={routeN === '' || routeN === null || routeN === undefined ? handleSubmit : handleRouteSubmit}>
                     <input
                         type="text"
                         onChange={e => setSearch(e.target.value)}
                         placeholder="Search"
-                        className="text-black text-lg leading-6 whitespace-nowrap bg-zinc-100 grow px-5 py-4 rounded-[50px] w-[30vw] max-md:max-w-full max-md:pl-1.5"
+                        disabled={loading}
+                        className="text-black disabled:brightness-50 text-lg leading-6 whitespace-nowrap bg-zinc-100 grow px-5 py-4 rounded-[50px] w-[30vw] max-md:max-w-full max-md:pl-1.5"
                     />
                 </form>
                 <img
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -22,11 +22,12 @@ function Results({ routeName }) {
     const [res, setRes] = React.useState([])
     const { data, isLoading, error } = useSWR('https://sheet.best/api/sheets/249b0e30-cf60-4ba8-9012-c5986052c0d1', fetcher)
     const searchParams = new URLSearchParams(window.location.search)
+    const [loader, setLoader] = React.useState(false)
 
     if (error) return <div className='w-full h-screen flex justify-center py-4 bg-[#EFEFEF]'>failed to load</div>
     if (isLoading) return (
         <>
-            <HeaderSection />
+            <HeaderSection routeN={routeName} loading={true} />
             <div className='w-full h-screen flex justify-center py-4 text-5xl font-bold bg-[#EFEFEF]'>
                 loading
                 <div className="flex flex-col gap-4 w-52">
@@ -39,12 +40,14 @@ function Results({ routeName }) {
         </>
     )
 
+
     if (routeName === "") return (
         <div>
-            <HeaderSection />
+            <HeaderSection routeN={routeName} loading={false} />
             <div className='w-full h-auto flex justify-center py-4 bg-[#EFEFEF]'>
                 <div className='w-[90%] h-full lg:flex md:block gap-2 '>
                     <div className='w-[30%] flex lg:flex-col md:flex-row gap-4 mr-4 pt-4'>
+
 
                         {routeName === "Audiovisual Media" && <h1 className='whitespace-normal text-4xl text-end'>AudioVisual <br /> <strong>Media</strong></h1>}
                         {routeName === "Visual Arts" && <h1 className='whitespace-normal text-4xl text-end'>Visual <br /> <strong>Arts</strong></h1>}
@@ -93,7 +96,7 @@ function Results({ routeName }) {
 
     if (data.filter(data => data.CreativeField_2 === routeName).length === 0) return (
         <>
-            <HeaderSection />
+            <HeaderSection routeN={routeName} loading={false} />
             <div className='w-full h-screen p-10 justify-center py-4 text-5xl font-bold bg-[#EFEFEF]'>
                 No results
                 <div className="flex flex-col gap-4 w-full p-10 mt-10">
@@ -116,18 +119,59 @@ function Results({ routeName }) {
 
     if (data.filter(data => data.CreativeField_2 === routeName).length !== 0) return (
         <div>
-            <HeaderSection />
+            <HeaderSection routeN={routeName} loading={false} />
             <div className='w-full h-auto flex justify-center py-4 bg-[#EFEFEF]'>
                 <div className='w-[90%] h-full lg:flex md:block gap-2 '>
                     <div className='w-[30%] flex lg:flex-col md:flex-row gap-4 mr-4 pt-4'>
 
-                        {routeName === "Audiovisual Media" && <h1 className='whitespace-normal text-4xl text-end'>AudioVisual <br /> <strong>Media</strong></h1>}
-                        {routeName === "Visual Arts" && <h1 className='whitespace-normal text-4xl text-end'>Visual <br /> <strong>Arts</strong></h1>}
-                        {routeName === "Creative Services" && <h1 className='whitespace-normal text-4xl text-end'>Creative <br /> <strong>Services</strong></h1>}
-                        {routeName === "Digital Interactive Media" && <h1 className='whitespace-normal text-4xl text-end'>Digital Interactive <br /> <strong>Media</strong></h1>}
-                        {routeName === "Design" && <h1 className='whitespace-normal text-4xl text-end'> <br /> <strong>Design</strong></h1>}
-                        {routeName === "Performing Arts" && <h1 className='whitespace-normal text-4xl text-end'>Performing <br /> <strong>Arts</strong></h1>}
-                        {routeName === "Cultural Arts" && <h1 className='whitespace-normal text-4xl text-end'>Cultural <br /> <strong>Arts</strong></h1>}
+                        {routeName === "Audiovisual Media" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>AudioVisual <br /> <strong>Media</strong></h1>)
+                            : routeName === "Audiovisual Media" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in AudioVisual Media</strong></h1>)
+                                : null}
+
+                        {routeName === "Visual Arts" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Visual <br /> <strong>Arts</strong></h1>)
+                            : routeName === "Visual Arts" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Visual Arts</strong></h1>)
+                                : null}
+
+                        {routeName === "Creative Services" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Creative <br /> <strong>Services</strong></h1>)
+                            : routeName === "Creative Services" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Creative Services</strong></h1>)
+                                : null}
+
+                        {routeName === "Digital Interactive Media" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Digital Interactive <br /> <strong>Media</strong></h1>)
+                            : routeName === "Digital Interactive Media" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Digital Interactive Media</strong></h1>)
+                                : null}
+
+                        {routeName === "Design" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'> <br /> <strong>Design</strong></h1>)
+                            : routeName === "Design" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Design</strong></h1>)
+                                : null}
+
+                        {routeName === "Performing Arts" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Performing <br /> <strong>Arts</strong></h1>)
+                            : routeName === "Performing Arts" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Performing Arts</strong></h1>)
+                                : null}
+
+                        {routeName === "Cultural Arts" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Cultural <br /> <strong>Arts</strong></h1>)
+                            : routeName === "Cultural Arts" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Cultural Arts</strong></h1>)
+                                : null}
+
+                        {routeName === "Publishing and Printed Media" && searchParams.get('search') === null
+                            ? (<h1 className='whitespace-normal text-4xl text-end'>Publishing and<br /><strong>Printed Media</strong></h1>)
+                            : routeName === "Publishing and Printed Media" && searchParams.get('search')
+                                ? (<h1 className='whitespace-normal text-4xl text-end'>Search results for {searchParams.get('search')} <br /> <strong>in Publishing and Printed Media</strong></h1>)
+                                : null}
+
                         <a className='flex self-end' href="/">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -155,7 +199,7 @@ function Results({ routeName }) {
                                 </div>
                             )
                         })}
-                        {routeName !== "" && data.filter(data => data.CreativeField_2 === routeName).map((items, i) => {
+                        {routeName !== "" && searchParams.get('search') === null ? data.filter(data => data.CreativeField_2 === routeName).map((items, i) => {
                             return (
                                 <div className='card lg:w-[80%] md:w-full flex bg-white rounded-2xl flex-col justify-center items-center p-4' key={i}>
                                     <img loading='lazy' src={items.profilephoto} className='mask mask-circle mb-4 object-cover self-center w-36 h-36' alt={i} />
@@ -165,6 +209,7 @@ function Results({ routeName }) {
                                         <p>{items.Location_2}</p>
                                         <h1>{items.MobileNumber}</h1>
                                         <h1>{items.EmailAddress}</h1>
+                                        {console.log(searchParams.get('search'))}
                                     </div>
                                     <div className="flex gap-2">
                                         {items.Facebook !== "" && (<a href={items.Facebook} target="_blank"><img src="/facebooklogo.png" alt="fblogo" /></a>)}
@@ -173,7 +218,27 @@ function Results({ routeName }) {
                                     </div>
                                 </div>
                             )
-                        })}
+                        }) : null}
+                        {routeName !== "" && searchParams.get('search') !== null ? data.filter(data => data.CreativeField_2 === routeName).filter(data => (data.FirstName + data.LastName).includes(searchParams.get('search'))).map((items, i) => {
+                            return (
+                                <div className='card lg:w-[80%] md:w-full flex bg-white rounded-2xl flex-col justify-center items-center p-4' key={i}>
+                                    <img loading='lazy' src={items.profilephoto} className='mask mask-circle mb-4 object-cover self-center w-36 h-36' alt={i} />
+                                    <h1 className='card-title'>{items.FirstName} {items.LastName}</h1>
+                                    <div className='card-body'>
+                                        <p>{items.shortbio}</p>
+                                        <p>{items.Location_2}</p>
+                                        <h1>{items.MobileNumber}</h1>
+                                        <h1>{items.EmailAddress}</h1>
+                                        {console.log(searchParams.get('search'))}
+                                    </div>
+                                    <div className="flex gap-2">
+                                        {items.Facebook !== "" && (<a href={items.Facebook} target="_blank"><img src="/facebooklogo.png" alt="fblogo" /></a>)}
+                                        {items.instagramlink !== "" && (<a href={items.instagramlink} target="_blank"><img src="/instagramlogo.png" alt="fblogo" /></a>)}
+                                        {items.Behance !== "" && (<a href={items.Behance} target="_blank"><img src="/behancelogo.svg" alt="fblogo" /></a>)}
+                                    </div>
+                                </div>
+                            )
+                        }) : null}
                     </div>
 
                 </div>
